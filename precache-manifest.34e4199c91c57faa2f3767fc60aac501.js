0c170853e2c8e3d5a82dc61765eb8e76526c69af
burhan <burhanrampura786@gmail.com>
2022-07-10 19:15:43 +0530

    Updates

---
+++ precache-manifest.34e4199c91c57faa2f3767fc60aac501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67d5c42319367b833437",
+    "url": "/Textutils-React/static/css/main.7cd550eb.chunk.css"
+  },
+  {
+    "revision": "67d5c42319367b833437",
+    "url": "/Textutils-React/static/js/main.67d5c423.chunk.js"
+  },
+  {
+    "revision": "5792cef5848f94440151",
+    "url": "/Textutils-React/static/js/1.5792cef5.chunk.js"
+  },
+  {
+    "revision": "5fae5ffb98b8db49e16d",
+    "url": "/Textutils-React/static/js/2.5fae5ffb.chunk.js"
+  },
+  {
+    "revision": "c18b5002218170ab8417",
+    "url": "/Textutils-React/static/js/runtime~main.c18b5002.js"
+  },
+  {
+    "revision": "ec1eead8b2607038edbff75d3e976687",
+    "url": "/Textutils-React/index.html"
+  }
+];
